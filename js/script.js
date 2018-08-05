8cdc587294f68f7b9a2653f44a23bb5858fc8a67
nokia6680 <artur.ballod@gmail.com>
2018-08-06 01:52:15 +0200

    #nightly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function Selected(a) {
   var label = a.value;
     if (label=="enter") {
-      (document.getElementById("form-enter").style.display="block") && (document.getElementById("agreement").style.display="none");
+      (document.getElementById("form-enter").style.display="block") &&
+      (document.getElementById("agreement").style.display="none") &&
+      (document.getElementById("enter-btn").style.display="block") &&
+      (document.getElementById("register-btn").style.display="none");
     } else {
-      (document.getElementById("form-enter").style.display="none") && (document.getElementById("agreement").style.display="block");
+      (document.getElementById("form-enter").style.display="none") &&
+      (document.getElementById("agreement").style.display="block") &&
+      (document.getElementById("register-btn").style.display="block") &&
+      (document.getElementById("enter-btn").style.display="none");
   } if (label=="register") {
       document.getElementById("form-register").style.display="block";
   } else {
