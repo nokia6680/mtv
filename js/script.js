15551136308ab0d9ad4d5143fba778ad7aab7d2d
nokia6680 <artur.ballod@gmail.com>
2018-08-06 04:25:10 +0200

    #что-то и нечто

--- js/script.js
+++ js/script.js
@@ -16,3 +16,33 @@ function Selected(a) {
       document.getElementById("form-register").style.display="none";
   }
 }
+
+
+var modalSearch = document.querySelector(".slide-panel");
+var closeButton = document.querySelector(".slide-panel__close");
+var openButton = document.querySelectorAll(".cabinet-details__photo");
+
+if (modalSearch) {
+  for (var i = 0; i < openButton.length; i++) openButton[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    modalSearch.classList.add("slide-panel--on");
+  });
+
+  modalSearch.addEventListener("click", function() {
+    modalSearch.classList.remove("slide-panel--on");
+  });
+
+  modalSearch.addEventListener("click", function(event) {
+    event.stopPropagation();
+  });
+
+  closeButton.addEventListener("click", function() {
+    modalSearch.classList.remove("slide-panel--on");
+  });
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      modalSearch.classList.remove("slide-panel--on");
+    }
+  });
+}
